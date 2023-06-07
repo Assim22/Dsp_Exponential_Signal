b8dcf2c3084fcaf372420de271d8e15b603c8141
ABNOUR II <119833997+Assim22@users.noreply.github.com>
2023-06-07 18:56:13 +0300

    Add files via upload

---
+++ exp.m
@@ -0,0 +1,14 @@
+% Name Assim Abdelnour
+clc;
+clear all: %delete all pervious variable
+close all % close all space work 
+%% Generation Signal
+t = 0:0.1:6;
+x = exp(t)
+%plot(t,x)
+stem(x)
+xlabel('time');
+ylabel('Amplitude');
+title('Exponential Signal');
+%% Generation sign
+
